43134e50d55904206a2f420f1a63f152b1cba17e
mfrancisco <francisco.melanie@gmail.com>
2022-06-02 14:29:49 -0700

    update App.js and add useState hook to add state

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import './SkillListItem.css'
 import './NewSkillForm.css'
 import SkillList from './SkillList';
 import NewSkillForm from './NewSkillForm'
+import {useState} from 'react';
 
 function App() {
-  const skills = [
-    { name: 'HTML', level: 5 },
-    { name: 'CSS', level: 3 },
-    { name: 'JavaScript', level: 4 },
-    { name: 'Python', level: 2 }
-  ];
-    return (
-      <div className="App">
-        <h1 className="teal-text">React Dev Skills</h1>
-        <SkillList skills={skills}/>
-        <hr></hr>
-        <NewSkillForm/>
-      </div>
-    );
+  const [skills, setSkills] = useState (
+    [
+      { name: 'HTML', level: 5 },
+      { name: 'CSS', level: 3 },
+      { name: 'JavaScript', level: 4 },
+      { name: 'Python', level: 2 }
+    ]
+  );
+
+  return (
+    <div className="App">
+      <h1 className="teal-text">React Dev Skills</h1>
+      <SkillList skills={skills}/>
+      <hr></hr>
+      <NewSkillForm/>
+    </div>
+  );
 }
 
 export default App;
